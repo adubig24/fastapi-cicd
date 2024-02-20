c9fe1962c3b4fcd0523ba18a9aa3db9a452f7e96
adubig24 <andy.dubigny@gmail.com>
2024-02-20 11:35:14 +0100

    ec2 1st try

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 # Import des librairies uvicorn, pickle, FastAPI, File, UploadFile, BaseModel
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Depends, Body
 import uvicorn
 import numpy as np 
 from pydantic import BaseModel
@@ -15,15 +15,15 @@ import boto3
 tags = [
        {
               "name": "Hello name V1",
-              "description": "Hello World",
+              "description": "Endpoint pour dire bonjour avec un nom en paramètre.",
        },
        {
               "name": "Predict V1",
-              "description": "Predict V1",
+              "description": "Endpoint pour prédire un résultat à partir du modèle 1 avec plusieurs caractéristiques.",
        },
        {
               "name": "Predict V2",
-              "description": "Predict V2",
+              "description": "Endpoint pour prédire un résultat à partir du modèle 2 avec moins de caractéristiques.",
        },
 ]
 
@@ -54,21 +54,51 @@ le_gender = loaded_data["le_gender"]
 # Point de terminaison avec paramètre
 @app.get("/hello", tags=["Hello name V1"])
 def hello(name: str='World'):
-        return {"message": f"Hello {name}"}
+    """
+    Endpoint permettant de saluer avec un message personnalisé.
+
+    Args:
+       name (str): Le nom à saluer. Par défaut, 'World'.
+
+    Returns:
+       dict: Un dictionnaire contenant le message de salutation.
+    """
+    return {"message": f"Hello {name}"}
 
 # Création du modèle de données pour le modéle 1 ('Gender', 'Age', 'Physical Activity Level', 'Heart Rate', 'Daily Steps', 'BloodPressure_high', 'BloodPressure_low', 'Sleep Disorder'])
 class Credit(BaseModel):
-        Gender : str
-        Age : int
-        Physical_Activity_Level : int
-        Heart_Rate : int
-        Daily_Steps : int
-        BloodPressure_high : int
-        BloodPressure_low : int
+    Gender : str
+    Age : int
+    Physical_Activity_Level : int
+    Heart_Rate : int
+    Daily_Steps : int
+    BloodPressure_high : int
+    BloodPressure_low : int
         
 # Point de terminaison : Prédiction 1
 @app.post("/predict", tags=["Predict V1"])
 def predict(credit: Credit):
+    """
+    Args:
+
+       - Gender (str): 'Male' ou 'Female'
+
+       - Age (int): Age de la personne.
+
+       - Physical_Activity_Level:  Niveau d'activité physique.
+
+       - Heart_Rate : Fréquence cardiaque.
+
+       - Daily_Steps : Nombre de pas effectué par jour.
+
+       - BloodPressure_high : La pression artérielle systolique.
+
+       - BloodPressure_low : La pression artérielle diastolique.
+
+       _______________________________________________________________________  
+    Returns:
+       dict: Un dictionnaire contenant le résultat de la prédiction.
+    """
     gender_numerical = le_gender.transform([credit.Gender])[0]
     
     X_new = [[gender_numerical, credit.Age, credit.Physical_Activity_Level,
@@ -79,16 +109,28 @@ def predict(credit: Credit):
     prediction_result_categorical = le_sleep.inverse_transform([prediction_result])
     return {"prediction": prediction_result_categorical.tolist()}
 
-
 # Création du modèle de données pour le modéle 2 ('Physical Activity Level', 'Heart Rate', 'Daily Steps', 'Sleep Disorder')
 class Credit_2(BaseModel):
-        Physical_Activity_Level : int
-        Heart_Rate : int
-        Daily_Steps : int
+    Physical_Activity_Level : int
+    Heart_Rate : int
+    Daily_Steps : int
 
 # Point de terminaison : Prédiction 2
 @app.post("/predict2", tags=["Predict V2"])
-def predict(credit: Credit_2):    
+def predict2(credit: Credit_2):
+    """
+    Args:
+
+       - Physical_Activity_Level:  Niveau d'activité physique.
+
+       - Heart_Rate : Fréquence cardiaque.
+
+       - Daily_Steps : Nombre de pas effectué par jour.
+
+       _______________________________________________________________________  
+    Returns:
+       dict: Un dictionnaire contenant le résultat de la prédiction.
+    """
     X_new = [[credit.Physical_Activity_Level, credit.Heart_Rate, credit.Daily_Steps]]
     
     prediction_result = svm_model_2.predict(X_new)
@@ -97,4 +139,4 @@ def predict(credit: Credit_2):
 
 # Démarage de l'application
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
